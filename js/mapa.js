357c800d9918b8c2a72684a7d0354ba8e7359e56
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-01 14:48:16 -0300

    Fixando os erros no template de mapas e cadastro de usuário

--- js/mapa.js
+++ js/mapa.js
@@ -1,11 +1,12 @@
-const itemMapa = document.getElementById("mapa-medicamento");
-let mapa = null;
+(function () {
+    const itemMapa = document.getElementById("mapa-medicamento");
+    let mapa = null;
 
-function iniciarMapa() {
-    mapa = new google.maps.Map(itemMapa, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-
-window.initMap = iniciarMapa;
+    function iniciarMapa() {
+        mapa = new google.maps.Map(itemMapa, {
+            center: { lat: -19.912998, lng: -43.940933 },
+            zoom: 8,
+        });
+    }    
+    window.initMap = iniciarMapa;    
+})();
