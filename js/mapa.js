81a29a32dc608f8823151fb9a9ed2c763b48f36a
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-01 13:13:25 -0300

    Fixando bug do mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,11 +1,11 @@
 const itemMapa = document.getElementById("mapa-medicamento");
 let mapa = null;
 
-function initMap() {
+function iniciarMapa() {
     mapa = new google.maps.Map(itemMapa, {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
     });
 }
 
-window.initMap = initMap;
+window.initMap = iniciarMapa;
