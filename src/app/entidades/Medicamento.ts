2c85087a4ad9067c744607ec650a97e495b2e2aa
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-14 01:46:17 +0200

    Atualizando entidade de medicamento

--- src/app/entidades/Medicamento.ts
+++ src/app/entidades/Medicamento.ts
@@ -1,5 +1,7 @@
 export class Medicamento {
+    public principioAtivo : string = null;
+    public laboratorioNome : string = null;
     public nome : string = null;
-    public dosagem : string = null;
-    public disponibilidade : boolean = null;
+    public apresentacao : string = null;
+    public tarja : string = null;
 }
