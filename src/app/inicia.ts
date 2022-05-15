e20b7bc6ee77ba32952a6ed1ec39192a42bee643
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-15 04:10:29 +0200

    Adicionando Regras de Script de construção e importação em JS

--- src/app/inicia.ts
+++
@@ -1,5 +0,0 @@
-import { MedicamentoControlador } from "./controladoras/MedicamentoControlador";
-import { MedicamentoRepositorio } from "./repositorios/MedicamentoRepositorio";
-
-const controle = new MedicamentoControlador();
-controle.buscarMedicamento('dipiroN');
