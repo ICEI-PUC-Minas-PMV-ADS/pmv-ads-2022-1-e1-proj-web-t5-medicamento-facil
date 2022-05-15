4fa5e0d7872d6986aec621b31dbbb4ccb3adcd38
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-16 01:40:22 +0200

    Adicionando regras de busca ao evento de tela

--- src/app/paginas/busca.medicamento.ts
+++ src/app/paginas/busca.medicamento.ts
@@ -1,9 +1,44 @@
 import { MedicamentoControlador } from "../controladoras/MedicamentoControlador";
+import { Medicamento } from "../entidades/Medicamento";
 
-const btnBusca = document.getElementById('buscar-medicamento');
-btnBusca.addEventListener('click', () => {
-    const input = document.getElementById('input-medicamento') as HTMLInputElement;
-    
-    const controle = new MedicamentoControlador();
-    controle.buscarMedicamento(input.value);
-});
+///// FUNCIONALIDADES
+const iniciaSelect2 = () => {
+    const seletor = document.getElementById('seletor-medicamento');
+    $(seletor).select2({
+        placeholder: "Buscar medicamentos",
+        multiple: true
+    });
+}
+
+let timer = null;
+const buscaMedicamento = () => {
+    const area = document.getElementById('area-busca');
+    const input = area.getElementsByTagName('input')[0];
+    const texto = input.value;
+
+    if (texto.length < 3) {
+        return;
+    }
+
+    if (timer) {
+        clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+
+        const controle = new MedicamentoControlador();
+        const medicamentos = controle.buscarMedicamento(texto);
+
+        medicamentos.forEach((item: Medicamento, id: number) => {
+            const opcao = new Option(item.nome, id.toString(), false, false);
+            const seletor = document.getElementById('seletor-medicamento');
+            $(seletor).select2().append(opcao).trigger('change');
+        })
+
+    }, 1000);
+}
+
+///// EVENTOS
+
+$(document).ready(iniciaSelect2);
+$('body').on('keyup', '.select2-search__field', buscaMedicamento);
