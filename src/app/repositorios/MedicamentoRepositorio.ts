eaae335c65fa547ee96caeae07ec72f213ea2018
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-05-15 02:32:22 +0200

    Otimizando a busca de medicamentos

--- src/app/repositorios/MedicamentoRepositorio.ts
+++ src/app/repositorios/MedicamentoRepositorio.ts
@@ -1,14 +1,36 @@
-import { medicamentos } from "../mocks/medicamentos.mock";
+import fs from 'fs';
+import { Medicamento } from '../entidades/Medicamento';
+import { medicamentos } from '../mocks/medicamentos.mock.min';
+import { Texto } from '../utils/Texto';
 
 export class MedicamentoRepositorio {
 
     public buscarMedicamentoPorNome(nome: string): Array<any> {
-        const medicamentosFiltrados = medicamentos.filter(x => x.nome === nome);
+        const busca: string = Texto.tirarAcentos(nome.toUpperCase());
+        const medicamentosFiltrados = medicamentos.filter(item => Texto.tirarAcentos(item.nome).search(busca) > -1);
         return medicamentosFiltrados;
     }
 
     public buscarMedicamentoDosagem(dosagem: string) {
 
+    }
+
+    public reduzirTamanhoDoMock(): void {
+
+
+        const medicamentosReduzidos = medicamentos.map(item => {
+            const medicamento = new Medicamento();
+            medicamento.apresentacao = item.apresentacao;
+            medicamento.laboratorioNome = item.laboratorioNome;
+            medicamento.nome = item.nome;
+            medicamento.principioAtivo = item.principioAtivo;
+            medicamento.tarja = item.tarja;
+
+            return medicamento;
+        });
+
+        const medicamentosJSON = JSON.stringify(medicamentosReduzidos);
+        fs.writeFileSync('medicamentos.json', medicamentosJSON);
     }
 
 }
