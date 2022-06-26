3f0e208ed1a3cbdefff9c9285924e6f1d1520804
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-06-26 03:43:20 +0200

    Adicionando regra de comunicação de usuário

---
+++ src/js/area-usuario.js
@@ -0,0 +1,11 @@
+
+    let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+
+   /* if(!userLogado){        
+        alert('Você precisa estar cadastrado em nossa base de dados.');
+        window.location.href = 'login.html';
+    } */
+
+    let logado = document.querySelector("#logado");
+
+    logado.innerHTML = `Olá, ${userLogado.nome}, o que você deseja?`
