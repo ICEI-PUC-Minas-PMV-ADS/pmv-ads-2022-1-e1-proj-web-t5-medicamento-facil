2ab7e8943ee5610b619221b99af754171629900a
Ríder Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-06-26 03:43:04 +0200

    Adicionando página inicial e login

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
 let endereco_google;
-        function initAutocomplete(){
-        endereco_google = new google.maps.places.Autocomplete(
-            document.getElementById('autocomplete'),
-            {
-                types: ['address'],
-                componentRestrictions: {'country': ['BR']},
-                fields: ['address_components']
-            })   ;
-    }
-     
-        
+function initAutocomplete() {
+    endereco_google = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            types: ['address'],
+            componentRestrictions: { 'country': ['BR'] },
+            fields: ['address_components']
+        });
+}
+
